7ac574f0fd4420ceb54b5f888fdc2191ecb2eb47
felipefacundes <felipe.facundes@gmail.com>
2019-03-16 17:41:26 -0300

    Update random-wallpaper.sh

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -1,22 +1,21 @@
 #!/bin/bash --norc
 # Comando de papel de parede aleatório universal, super simples. Random Wallpaper Easy.
-# Licença GPLv2 
-# Mantenedor Felipe Facundes 
+# Licença GPLv2
+# Mantenedor Felipe Facundes
 # e-mail: felipe.facundes@gmail.com
 
-# Sinal 'kill' recebido
-#trap "break ; exit" 1 2 3 15
+# Sinal 'kill' recebido | Descomente a linha de baixo caso use o script, diretamente no ~/.xinitrc
+# trap "break ; exit" 1 2 3 15
 
 # Defina um tempo mínimo de 15 segundos, antes de executar o script. Isso evita e corrige erros.
 sleep 15
 
-# Diretório da sua preferência
-DIR=/home/rfacundes/Imagens/Tudo/wallpapers/jpg/
-
-# O padrão: -name "*[jJpP][nNpP][gG]" serve para pesquisar jpg e png. Mude para "-type f" caso queira qualquer tipo arquivo.
+# O padrão: -name "*[jJpP][nNpP][gG]" serve para pesquisar jpg e png. Mude para "-type f" caso queira qualquer tipo de arquivo.
 while true;
+# DIR = Diretório da sua preferência
+do export DIR="/home/$USER/Imagens/Tudo/wallpapers/jpg/"
 # Mude para o comando correspondente listado abaixo, conforme à sua área de trabalho: MATE, LXDE, Fluxbox e etc...
-do bash -c 'gsettings set org.mate.background picture-filename "$(find $DIR -name "*[jJpP][nNpP][gG]" | shuf -n1)"';
+bash -c 'gsettings set org.mate.background picture-filename "$(find $DIR -name "*[jJpP][nNpP][gG]" | shuf -n1)"';
 # Atualizando tela:
 xrefresh
 # Mude para o tempo que você quiser
