c209a9417fe7e3c1710020066b37619d90af1d3e
felipefacundes <felipe.facundes@gmail.com>
2019-03-16 16:20:46 -0300

    Update random-wallpaper.sh

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -5,20 +5,29 @@
 # e-mail: felipe.facundes@gmail.com
 
 # Sinal 'kill' recebido
-trap "break ; exit" 1 2 3 15
+#trap "break ; exit" 1 2 3 15
 
-# Após o comando: "find" troque o diretório da "pasta" dos papéis de parede, para o seu diretório correspondente
+# Defina um tempo mínimo de 15 segundos, antes de executar o script. Isso evita e corrige erros.
+sleep 15
+
+# Diretório da sua preferência
+DIR=/home/rfacundes/Imagens/Tudo/wallpapers/jpg/
+
+# O padrão: -name "*[jJpP][nNpP][gG]" serve para pesquisar jpg e png. Mude para "-type f" caso queira qualquer tipo arquivo.
 while true;
 # Mude para o comando correspondente listado abaixo, conforme à sua área de trabalho: MATE, LXDE, Fluxbox e etc...
-do bash -c 'gsettings set org.mate.background picture-filename "$(find $HOME/Imagens/Tudo/wallpapers/jpg/ -type f | shuf -n1)"';
+do bash -c 'gsettings set org.mate.background picture-filename "$(find $DIR -name "*[jJpP][nNpP][gG]" | shuf -n1)"';
 # Atualizando tela:
-xrefresh || :
+xrefresh
 # Mude para o tempo que você quiser
 sleep 15
 done;
 
 # Para MATE Desktop
 # gsettings set org.mate.background picture-filename
+
+# Para GNOME
+# gsettings set org.gnome.desktop.background picture-uri
 
 # Para LXDE Desktop
 # pcmanfm -w
@@ -29,8 +38,5 @@ done;
 # Para NITROGEN
 # nitrogen --set-zoom-fill
 
-# Para FEH
-# feh --bg-scale
-
 # Para png e jpg
-# *.[jJpP][nNpP][gG]
+# *[jJpP][nNpP][gG]
